49277102328ac452e284349f8e560808ccc154ed
Saleemf340 <saleemf340@gmail.com>
2023-07-13 09:42:51 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Result: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is piece of art,");
+	return (0);
+}
