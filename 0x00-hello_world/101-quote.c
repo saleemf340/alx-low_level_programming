ea1fe0e9d31e9eac93db5f1a5ef3757705dbbb36
Saleemf340 <saleemf340@gmail.com>
2023-07-13 12:53:51 -0700

    fix again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,11 @@
  *
  * Return 1 always
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-	   return 1; 
+	char *s;
+
+	s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, s, 59);
+	return (1);
 }
