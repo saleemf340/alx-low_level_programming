67df2a49a148c2032964a5696f25ae81051f906c
Saleemf340 <saleemf340@gmail.com>
2023-07-13 12:37:50 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+/**
+ * main - starting point
+ *
+ * Return 1 always
+ */
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    	syscall(SYS_write, 1, msg, sizeof(msg));
+    	return 0;
+}
